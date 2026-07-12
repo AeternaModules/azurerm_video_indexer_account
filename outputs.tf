88befb4fd712919ca8668d71c83afcc5899619ff
Daniel Allen <dan@aeternagroup.net>
2026-07-12 14:21:16 +0100

    Update to schema version v4.80.0

--- outputs.tf
+++ outputs.tf
@@ -1,33 +1,29 @@
-output "video_indexer_accounts" {
-  description = "All video_indexer_account resources"
-  value       = azurerm_video_indexer_account.video_indexer_accounts
-}
 output "video_indexer_accounts_identity" {
-  description = "List of identity values across all video_indexer_accounts"
-  value       = [for k, v in azurerm_video_indexer_account.video_indexer_accounts : v.identity]
+  description = "Map of identity values across all video_indexer_accounts, keyed the same as var.video_indexer_accounts"
+  value       = { for k, v in azurerm_video_indexer_account.video_indexer_accounts : k => v.identity }
 }
 output "video_indexer_accounts_location" {
-  description = "List of location values across all video_indexer_accounts"
-  value       = [for k, v in azurerm_video_indexer_account.video_indexer_accounts : v.location]
+  description = "Map of location values across all video_indexer_accounts, keyed the same as var.video_indexer_accounts"
+  value       = { for k, v in azurerm_video_indexer_account.video_indexer_accounts : k => v.location }
 }
 output "video_indexer_accounts_name" {
-  description = "List of name values across all video_indexer_accounts"
-  value       = [for k, v in azurerm_video_indexer_account.video_indexer_accounts : v.name]
+  description = "Map of name values across all video_indexer_accounts, keyed the same as var.video_indexer_accounts"
+  value       = { for k, v in azurerm_video_indexer_account.video_indexer_accounts : k => v.name }
 }
 output "video_indexer_accounts_public_network_access" {
-  description = "List of public_network_access values across all video_indexer_accounts"
-  value       = [for k, v in azurerm_video_indexer_account.video_indexer_accounts : v.public_network_access]
+  description = "Map of public_network_access values across all video_indexer_accounts, keyed the same as var.video_indexer_accounts"
+  value       = { for k, v in azurerm_video_indexer_account.video_indexer_accounts : k => v.public_network_access }
 }
 output "video_indexer_accounts_resource_group_name" {
-  description = "List of resource_group_name values across all video_indexer_accounts"
-  value       = [for k, v in azurerm_video_indexer_account.video_indexer_accounts : v.resource_group_name]
+  description = "Map of resource_group_name values across all video_indexer_accounts, keyed the same as var.video_indexer_accounts"
+  value       = { for k, v in azurerm_video_indexer_account.video_indexer_accounts : k => v.resource_group_name }
 }
 output "video_indexer_accounts_storage" {
-  description = "List of storage values across all video_indexer_accounts"
-  value       = [for k, v in azurerm_video_indexer_account.video_indexer_accounts : v.storage]
+  description = "Map of storage values across all video_indexer_accounts, keyed the same as var.video_indexer_accounts"
+  value       = { for k, v in azurerm_video_indexer_account.video_indexer_accounts : k => v.storage }
 }
 output "video_indexer_accounts_tags" {
-  description = "List of tags values across all video_indexer_accounts"
-  value       = [for k, v in azurerm_video_indexer_account.video_indexer_accounts : v.tags]
+  description = "Map of tags values across all video_indexer_accounts, keyed the same as var.video_indexer_accounts"
+  value       = { for k, v in azurerm_video_indexer_account.video_indexer_accounts : k => v.tags }
 }
 
